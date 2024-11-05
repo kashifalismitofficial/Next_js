487d5cb7a57dcd228137aa46b159567e15e4f65a
kashifalismitofficial <kashifalismitofficial@gmail.com>
2024-11-05 05:34:55 +0500

    para-width

--- figma-assigment/src/app/page.tsx
+++ figma-assigment/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
         <div className="w-1/2">
           <h1 className="font-bold font-serif text-[40px] w-[496px]">IMPECCABLE CRAFTSMANSHIP AND FINESSE
           </h1>
-          <p className="mt-[40px] w-[700px] text-[28px] text-[#A29875] font-serif">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam quas, consequuntur consectetur dolorem impedit beatae magni repudiandae provident.</p>
+          <p className="mt-[40px] w-[600px] text-[28px] text-[#A29875] font-serif">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam quas, consequuntur consectetur dolorem impedit beatae magni repudiandae provident.</p>
           <button className="mt-[40px] bg-[#A29875] w-[288px] h-[58px] font-serif text-white text-[30px]">Explore Now</button>
         </div>
         <div>
